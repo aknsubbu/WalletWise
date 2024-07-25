6c62408504780ac350e931e6d117a3c11405bf34
aknsubbu <aknsubbu@gmail.com>
2024-07-25 15:53:30 +0530

    tailwind configed

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,22 @@
+import React from "react";
 import {
   StyleSheet,
   View,
   TouchableOpacity,
   Text,
   Dimensions,
+  SafeAreaView,
 } from "react-native";
-import { useEffect, useState } from "react";
-
-const { height, width } = Dimensions.get("window");
 
 const App = () => {
-  return <View></View>;
+  return (
+    <SafeAreaView>
+      <View className="p-5">
+        <Text className="text-white">Hi,</Text>
+        <Text></Text>
+      </View>
+    </SafeAreaView>
+  );
 };
 
 export default App;
